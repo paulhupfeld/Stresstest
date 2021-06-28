a0f269673801cec3e8f17f60bf90338e41dcb46b
paulhupfeld <paul.hupfeld@stud.h-da.de>
2021-06-28 22:40:13 +0200

    preload is working

--- javascript/task.js
+++ javascript/task.js
@@ -1,32 +1,31 @@
 import Button from "./button.js";
 
-// import
-//{ actorImage } from ".p5setup.js";
-
-actorImg = loadImage("theater/actorImage.png");
-//let boxesImg = loadImage("theater/Boxes.png");
-broomImg = loadImage("theater/Broom-n-bucket.png");
-theaterImg = loadImage("theater/Theater-Background.png");
-
-// chairboxImage = loadImage("theater/Chair-box.png");
-// chairsImage = loadImage("theater/Chairs.png");
-// chairsReservedImage = loadImage("theater/Chairs-reserved.png");
-// curtainLeft = loadImage("theater/Curtain-Left.png");
-// curtainRight = loadImage("theater/Curtain-right.png");
-// doorLeft = loadImage("theater/Door-Left.png");
-// doorRight = loadImage("theater/Door-Right.png");
-// exit = loadImage("theater/Exit.png");
-// konfetti = loadImage("theater/Konfetti.png");
-// reservedBox = loadImage("theater/Reserved-Box.png");
-// spotlightDown = loadImage("theater/Spotlight-down.png");
-// spotlightOff = loadImage("theater/Spotlight-off.png");
-// spotlightOn = loadImage("theater/Spotlight-on.png");
-// supportBeam = loadImage("theater/Support-beam.png");
-// teleprompterOff = loadImage("theater/Teleprompter-Off.png");
-// teleprompterOn = loadImage("theater/Teleprompter-On.png");
+import {
+  actorImage,
+  boxesImage,
+  broomImage,
+  chairboxImage,
+  chairsReservedImage,
+  chairsImage,
+  curtainLeftImage,
+  curtainRightImage,
+  doorLeftImage,
+  doorRightImage,
+  exitImage,
+  konfettiImage,
+  reservedBoxImage,
+  spotlightDownImage,
+  spotlightOffImage,
+  spotlightOnImage,
+  stageImage,
+  supportBeamImage,
+  teleprompterOffImage,
+  teleprompterOnImage,
+  theaterBackgroundImage,
+} from "../p5setup.js";
 
 class Task extends Button {
-  constructor(imageX, imageY, scale, hitboxX, hitboxY, width, height, img) {
+  constructor(imageX, imageY, scale, hitboxX, hitboxY, width, height, image) {
     super(hitboxX, hitboxY);
     this.imageX = imageX;
     this.imageY = imageY;
@@ -35,46 +34,58 @@ class Task extends Button {
     this.hitboxY = hitboxY;
     this.width = width;
     this.height = height;
-    this.img = img;
+    this.image = image;
   }
 
   display() {
-    // console.log("hi");
-    // image(actorImage, 100, 100, 100, 100);
-
     push();
     noStroke();
 
     translate(
-      this.imageX - (this.img.width / 2) * this.scale,
-      this.imageY - (this.img.height / 2) * this.scale
+      this.imageX - (this.image.width / 2) * this.scale,
+      this.imageY - (this.image.height / 2) * this.scale
     );
     //console.log(this.img);
 
     image(
-      this.img,
+      this.image,
       0,
       0,
-      this.img.width * this.scale,
-      this.img.height * this.scale
+      this.image.width * this.scale,
+      this.image.height * this.scale
     );
 
     pop();
-    // ellipse(this.x, this.y, 10);
   }
 }
 
-let actor = new Task(200, 200, 1, 180, 180, 160, 170, actorImg);
-let broom = new Task(400, 200, 1, 400, 200, 90, 190, broomImg);
+let start = true;
+let actor;
+let broom;
+let theaterBackground;
 
 function draw() {
-  image(theaterImg, 0, 0);
-  actor.display();
-  // console.log("hi");
-  actor.showHitbox();
+  if (start === true) {
+    actor = new Task(200, 200, 1, 180, 180, 160, 170, actorImage);
+    broom = new Task(200, 200, 1, 180, 180, 160, 170, spotlightDownImage);
+    theaterBackground = new Task(
+      640,
+      360,
+      1,
+      180,
+      180,
+      160,
+      170,
+      theaterBackgroundImage
+    );
 
-  broom.display();
-  broom.showHitbox();
+    start = false;
+  }
+  // image(theaterImg, 0, 0);
+  // actor.display();
+  // actor.showHitbox();
+  theaterBackground.display();
+  // theaterBackground.showHitbox();
 }
 
 function mouseClicked() {
--- p5setup.js
+++ p5setup.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(600, 595);
+  createCanvas(1280, 720);
   frameRate(30);
 }
 window.setup = setup;
@@ -13,12 +13,52 @@ window.addEventListener("resize", function () {
   clear();
 });
 
-window.preload = preload;
-
-let actorImage;
+export let actorImage;
+export let boxesImage;
+export let broomImage;
+export let chairboxImage;
+export let chairsReservedImage;
+export let chairsImage;
+export let curtainLeftImage;
+export let curtainRightImage;
+export let doorLeftImage;
+export let doorRightImage;
+export let exitImage;
+export let konfettiImage;
+export let reservedBoxImage;
+export let spotlightDownImage;
+export let spotlightOffImage;
+export let spotlightOnImage;
+export let stageImage;
+export let supportBeamImage;
+export let teleprompterOffImage;
+export let teleprompterOnImage;
+export let theaterBackgroundImage;
 
 function preload() {
   actorImage = loadImage("javascript/theater/actorImage.png");
+  boxesImage = loadImage("javascript/theater/Boxes.png");
+  broomImage = loadImage("javascript/theater/Broom-n-bucket.png");
+  chairboxImage = loadImage("javascript/theater/Chair-box.png");
+  chairsReservedImage = loadImage("javascript/theater/Chairs-reserved.png");
+  chairsImage = loadImage("javascript/theater/Chairs.png");
+  curtainLeftImage = loadImage("javascript/theater/Curtain-Left.png");
+  curtainRightImage = loadImage("javascript/theater/Curtain-right.png");
+  doorLeftImage = loadImage("javascript/theater/Door-Left.png");
+  doorRightImage = loadImage("javascript/theater/Door-Right.png");
+  exitImage = loadImage("javascript/theater/Exit.png");
+  konfettiImage = loadImage("javascript/theater/Konfetti.png");
+  reservedBoxImage = loadImage("javascript/theater/Reserved-Box.png");
+  spotlightDownImage = loadImage("javascript/theater/Spotlight-down.png");
+  spotlightOffImage = loadImage("javascript/theater/Spotlight-off.png");
+  spotlightOnImage = loadImage("javascript/theater/Spotlight-on.png");
+  stageImage = loadImage("javascript/theater/Stage.png");
+  supportBeamImage = loadImage("javascript/theater/Support-beam.png");
+  teleprompterOffImage = loadImage("javascript/theater/Teleprompter-Off.png");
+  teleprompterOnImage = loadImage("javascript/theater/Teleprompter-On.png");
+  theaterBackgroundImage = loadImage(
+    "javascript/theater/Theater-Background.png"
+  );
 }
 
-//export { actorImage };
+window.preload = preload;
