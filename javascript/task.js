44b9908a72a082fbe8e974fd5e2844f7a444cbf2
nesrinha <58940244+nesrinha@users.noreply.github.com>
2021-06-29 14:49:19 +0200

    Revert "Neue Klasse"
    This reverts commit f854731a736e6e5e05755cf94de9360ef599c267.

--- javascript/task.js
+++ javascript/task.js
@@ -1,4 +1,5 @@
-import TaskImage from "./taskImage.js";
+import Button from "./button.js";
+
 import {
   boxesImage,
   broomImage,
@@ -23,12 +24,43 @@ import {
   curtainClosedImage,
   emergencySignImage,
 } from "../p5setup.js";
-class Task {
-  constructor(title, time) {
-    this.title = title;
-    this.time = time;
-    this.prioboard = false;
-    this.done = false;
+
+class TaskImage extends Button {
+  constructor(imageX, imageY, scale, hitboxX, hitboxY, width, height, image) {
+    super(hitboxX, hitboxY);
+    this.imageX = imageX;
+    this.imageY = imageY;
+    this.scale = scale;
+    this.hitboxX = hitboxX;
+    this.hitboxY = hitboxY;
+    this.width = width;
+    this.height = height;
+    this.image = image;
+    // this.title = title;
+    // this.time = time;
+    // this.prioboard = false;
+    // this.done = false;
+  }
+
+  display() {
+    push();
+    noStroke();
+
+    translate(
+      this.imageX - (this.image.width / 2) * this.scale,
+      this.imageY - (this.image.height / 2) * this.scale
+    );
+    //console.log(this.img);
+
+    image(
+      this.image,
+      0,
+      0,
+      this.image.width * this.scale,
+      this.image.height * this.scale
+    );
+
+    pop();
   }
 }
 
