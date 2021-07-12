31cffa451dc3299577c8be09db78a30d943b95ae
geeeny <gina-marie.leuthner@stud.h-da.de>
2021-07-12 22:13:02 +0200

    3D blur effekt
    holy SHIIIIIIIIIIIIIIIIT

--- javascript/taskImage.js
+++ javascript/taskImage.js
@@ -11,18 +11,46 @@ export default class TaskImage extends Button {
     this.height = height;
     this.image = image;
     this.scale = 1;
+    this.blurrX = 0;
+    this.blurrY = 0;
   }
 
   display() {
     push();
+
     noStroke();
 
     translate(
       this.imageX - (this.image.width / 2) * this.scale,
       this.imageY - (this.image.height / 2) * this.scale
     );
-    //console.log(this.img);
 
+    /*
+    if (Math.random() > 0.8) {
+      //Hat die Framerate verlangsamt, als es konstant war, also mussten wir sparsamer damit umgehen
+      // Jetzt ist der Schaden minimal und der Effekt noch stressiger! #FailUpwards
+      this.blurrX = random(0, 30);
+      this.blurrY = random(0, 10);
+      tint(60, 170, 255, 200);
+      image(
+        this.image,
+        0 + this.blurrX,
+        0 + this.blurrY,
+        this.image.width * this.scale,
+        this.image.height * this.scale
+      );
+      tint(255, 60, 60, 200);
+      image(
+        this.image,
+        0 - this.blurrX,
+        0 - this.blurrY,
+        this.image.width * this.scale,
+        this.image.height * this.scale
+      );
+    }
+    */
+
+    noTint();
     image(
       this.image,
       0,
